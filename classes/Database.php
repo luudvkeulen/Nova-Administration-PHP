f50c6c56c3f059231e4bcbc23be1944170c1c272
luud <l.vankeulen@student.fontys.nl>
2017-03-04 20:31:10 +0100

    Added database functions

--- classes/Database.php
+++ classes/Database.php
@@ -33,6 +33,56 @@ class Database
     }
 
     public function bind($param, $value, $type = null) {
-        
+        if(is_null($type)) {
+            switch(true) {
+                case is_int($value):
+                    $type = PDO::PARAM_INT;
+                    break;
+                case is_bool($value):
+                    $type = PDO::PARAM_BOOL;
+                    break;
+                case is_null($value):
+                    $type = PDO::PARAM_NULL;
+                    break;
+                default:
+                    $type = PDO::PARAM_STR;
+            }
+        }
+
+        $this->stmt->bindValue($param, $value, $type);
+    }
+
+    public function execute() {
+        return $this->stmt->execute();
+    }
+
+    public function resultSet() {
+        $this->execute();
+        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function single(){
+        $this->execute();
+        return $this->stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function rowCount(){
+        return $this->stmt->rowCount();
+    }
+
+    public function lastInsertId(){
+        return $this->dbh->lastInsertId();
+    }
+
+    public function beginTransaction(){
+        return $this->dbh->beginTransaction();
+    }
+
+    public function endTransaction(){
+        return $this->dbh->commit();
+    }
+
+    public function cancelTransaction(){
+        return $this->dbh->rollBack();
     }
 }
